cf27338e93810a3bb70b671335c80b1527cd69cd
Vichaslav Kostianets <vyacheslav.kostyanets@gmail.com>
2022-05-01 19:24:08 +0300

    add button, input and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import InputField from './components/InputField'
 
 
 
 const App:React.FC = () => {
+
+  const [todo, setTodo] = useState("");
+
   return (
     <div className="App">
     <span className="heading">
